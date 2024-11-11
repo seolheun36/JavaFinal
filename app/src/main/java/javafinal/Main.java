624b7c8627a986f66c109128ab9c6161537c2dcb
seolheun5 <piberius5@gmail.com>
2024-11-12 01:16:50 +0900

    docs: Main.java javadoc 주석 추가

--- app/src/main/java/javafinal/Main.java
+++ app/src/main/java/javafinal/Main.java
@@ -1,6 +1,27 @@
 package javafinal;
 
+/**
+ * {@code Main} 클래스
+ * 
+ * @author seolheun5 (김은성, piberius5@gmail.com)
+ * 
+ * @create 2024-11-12
+ * @lastModified 2024-11-12
+ * 
+ * @changelog
+ * <ul>
+ * <li>2024-11-12: 최초 생성</li>
+ * </ul>
+ */
 public class Main {
+
+    /**
+     * {@cdoe main} 메서드
+     * 
+     * @author seolheun5
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         
     }
