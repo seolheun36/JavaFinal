ba3b4f100ef89915f41a040c5ad333a8f134f93c
seolheun5 <piberius5@gmail.com>
2024-11-12 10:28:33 +0900

    feat: 공지 게시물 제목 및 내용 추출기능 업데이트
    - 공지 게시물 제목 및 내용 추출
    - 제목 + 내용 추출 기능 출력 코드 작성

--- app/src/main/java/javafinal/logic/NoticeCrawler.java
+++ app/src/main/java/javafinal/logic/NoticeCrawler.java
@@ -23,21 +23,27 @@ public class NoticeCrawler {
     private String homepage = "https://www.cju.ac.kr/www/";
 
     /**
+     * {@code NoticeCrawler} 생성자
      * 
+     * @author seolheun5
      */
     NoticeCrawler() {
-        String infoUrl = homepage + "selectBbsNttList.do?key=4577&bbsNo=881&searchCnd=SJ&searchKrwd=결행";
+        String noticeListURL = homepage + "selectBbsNttList.do?key=4577&bbsNo=881&searchCnd=SJ&searchKrwd=결행";
         try {
-            Document doc = Jsoup.connect(infoUrl).get();
-            Elements infoBox = doc.select(".tb");
-            Elements infoLists = infoBox.select("tr");
+            Document doc = Jsoup.connect(noticeListURL).get();
+            Elements noticeTBody = doc.select(".tb");
+            Elements noticeTRs = noticeTBody.select("tr");
 
-            for (Element infoList : infoLists) {
-                String infoNum = infoList.select(".web_block").first().text();
+            for (Element noticeTR : noticeTRs) {
+                String infoNum = noticeTR.select(".web_block").first().text();
 
                 if (!infoNum.contains("[공지]")) {
-                    Elements infoSubjects = infoList.select(".subject");
-                    contentsCrawler(infoSubjects);
+                    Elements noticeSubjectss = noticeTR.select(".subject");
+                    
+                    String noticeSubject = noticeSubjectss.text();
+                    String noticeContent = contentsCrawler(noticeSubjectss);
+
+                    System.out.println(noticeSubject + "\n\n" + noticeContent);
                 }
             }
         } catch (IOException e) {
@@ -45,33 +51,41 @@ public class NoticeCrawler {
         }
     }
 
-    private void contentsCrawler(Elements infoSubjects) {
-        Elements infoLinks = infoSubjects.select("a");
+    /**
+     * {@code contentsCrawler} 메서드
+     * 
+     * @author seolheun5
+     * 
+     * @param infoSubjects 각 공지 내용 중 {@code class="subject"}인 요소를 담은 {@code Elements} 타입 자료
+     */
+    private String contentsCrawler(Elements noticeSubjects) {
+        Elements noticeLinks = noticeSubjects.select("a");
 
-        String subject = infoSubjects.text();
-        String link = infoLinks.attr("href").substring(2);
-        String listUrl = homepage + link;
+        String link = noticeLinks.attr("href").substring(2);
+        String noticeURL = homepage + link;
+
+        String notice = "";
 
         try {
-            Document doc = Jsoup.connect(listUrl).get();
+            Document doc = Jsoup.connect(noticeURL).get();
             Elements contents = doc.select(".bbs_content");
             Elements contentLines = contents.select(".0");
 
+            StringBuilder bld = new StringBuilder();
             for (Element element : contentLines) {
                 Elements contentTexts = element.select("span");
 
                 for (Element contentText : contentTexts) {
-                    System.out.print(contentText.wholeText());
+                    bld.append(contentText.wholeText());
                 }
-
-                System.out.println();
+                bld.append("\n");
             }
-
-            System.out.println("---");
-            System.out.println();
+            notice = bld.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+        return notice;
     }
 
     public static void main(String[] args) {
