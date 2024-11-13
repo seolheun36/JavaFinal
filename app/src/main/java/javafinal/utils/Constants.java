68a1b32afddbaf14cc3bdcad6c1847f6302a3d9c
seolheun5 <piberius5@gmail.com>
2024-11-13 21:24:12 +0900

    chore: 상수 관리를 위한 Constants 클래스 생성

---
+++ app/src/main/java/javafinal/utils/Constants.java
@@ -0,0 +1,9 @@
+package javafinal.utils;
+
+public class Constants {
+    // 폰트
+    public static final String FONT = "SansSerif";
+
+    // 텍스트
+    public static final String NOTICE_TITLE = "셔틀 결행 공지";
+}
