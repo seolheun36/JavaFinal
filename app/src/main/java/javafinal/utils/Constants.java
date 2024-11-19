4375d98e4fe01bc84660fd93c8b7390c53a67221
seolheun5 <piberius5@gmail.com>
2024-11-19 12:51:03 +0900

    chore: 유틸리티 클래스인 Constants의 인스턴스화 방지 처리

--- app/src/main/java/javafinal/utils/Constants.java
+++ app/src/main/java/javafinal/utils/Constants.java
@@ -12,9 +12,17 @@ package javafinal.utils;
  * <ul>
  * <li>2024-11-13: 최초 생성</li>
  * <li>2024-11-13: FONT 상수와 NOTICE_TITLE 상수 추가</li>
+ * <li>2024-11-19: 유틸리티 클래스에서 인스턴스화 방지</li>
  * </ul>
  */
 public class Constants {
+    /**
+     * 유틸리티 클래스인 {@code Constants}의 인스턴스화 방지 처리.
+     */
+    private Constants() {
+        throw new IllegalStateException("This is a Utility class");
+    }
+
     // 폰트
     public static final String FONT = "SansSerif";
 
