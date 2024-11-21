c82a269309f5975f35a1ed5ff7bfc6266aa93d3b
seolheun5 <piberius5@gmail.com>
2024-11-21 23:26:50 +0900

    feat: 스크롤바 디자인 수정
    - 스크롤바 바 디자인 수정
    - 스크롤바 바 색상 설정

--- app/src/main/java/javafinal/utils/CustomScrollBarUI.java
+++ app/src/main/java/javafinal/utils/CustomScrollBarUI.java
@@ -18,9 +18,11 @@ import javax.swing.plaf.basic.BasicScrollBarUI;
  * <ul>
  * <li>2024-11-21: 최초 생성</li>
  * <li>2024-11-21: 스크롤바 버튼 삭제</li>
+ * <li>2024-11-21: 스크롤바 디자인 및 색상 변경</li>
  * </ul>
  */
 public class CustomScrollBarUI extends BasicScrollBarUI {
+    // 버튼 설정
     @Override
     protected JButton createDecreaseButton(int orientation) {
         return createInvisibleButton();
@@ -37,5 +39,21 @@ public class CustomScrollBarUI extends BasicScrollBarUI {
         button.setMinimumSize(new Dimension(0, 0));
         button.setMaximumSize(new Dimension(0, 0));
         return button;
+    }
+
+    // 스크롤바 설정
+    @Override
+    protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
+        g.setColor(new Color(0x396ac3));
+        g.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
+    }
+
+    @Override
+    public Dimension getPreferredSize(JComponent c) {
+        if (scrollbar.getOrientation() == Adjustable.VERTICAL) {
+            return new Dimension(8, super.getPreferredSize(c).height);
+        } else {
+            return new Dimension(super.getPreferredSize(c).width, 10);
+        }
     }
 }
