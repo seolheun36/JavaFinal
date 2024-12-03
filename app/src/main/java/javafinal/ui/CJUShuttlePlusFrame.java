71684fab1c0327194d784ce6af58086e06dda404
seolheun5 <piberius5@gmail.com>
2024-12-03 09:32:37 +0900

    feat: 청주 셔틀 플러스 앱 아이콘 설정

--- app/src/main/java/javafinal/ui/CJUShuttlePlusFrame.java
+++ app/src/main/java/javafinal/ui/CJUShuttlePlusFrame.java
@@ -1,6 +1,9 @@
 package javafinal.ui;
 
-import javax.swing.*;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 /**
  * {@code CJUShuttlePlusFrame} 클래스는 앱의 패널들을 관리해주는 프레임입니다.
@@ -8,14 +11,17 @@ import javax.swing.*;
  * @author seolheun5
  * 
  * @create 2024-11-13
- * @lastModified 2024-11-19
+ * @lastModified 2024-12-03
  * 
  * @changelog
  * <ul>
  * <li>2024-11-13: 최초 생성</li>
  * <li>2024-11-13: 프레임 기본 설정</li>
  * <li>2024-11-19: 셔틀 결행 패널 클래스 이름 변경에 따른 수정</li>
+ * <li>2024-12-03: 앱 아이콘 설정</li>
  * </ul>
+ * 
+ * @see <a href="https://www.flaticon.com/uicons">Flaticon</a>의 UIcon
  */
 public class CJUShuttlePlusFrame extends JFrame {
     public CJUShuttlePlusFrame() {
@@ -23,6 +29,9 @@ public class CJUShuttlePlusFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(400, 700);
         setLocationRelativeTo(null);
+
+        Image icon = new ImageIcon(getClass().getResource("../resources/icon/busIcon.png")).getImage();
+        setIconImage(icon);
 
         CJUShuttlePlusNoticePanel noticePanel = new CJUShuttlePlusNoticePanel();
         add(noticePanel);
