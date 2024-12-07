5ca58f8bb850ba431c5db0597e9e120e0c02b574
seolheun5 <piberius5@gmail.com>
2024-12-08 00:08:58 +0900

    docs: javadoc 파일 추가

---
+++ app/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"javafinal"},{"l":"javafinal.logic"},{"l":"javafinal.ui"},{"l":"javafinal.utils"}];updateSearchResults();
